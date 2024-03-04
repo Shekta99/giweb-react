3d3e49b8133bd1c99cc51189581ce13ebb287b60
Sergio Marentes <sergiomarentes705@gmail.com>
2024-03-04 10:03:15 -0500

    changing the message to hello word

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <p>
-        Hello word
-      </p>
+        <p>Hello word</p>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
